fe5b08299d2aec84bef0e653837d5058a9e0e02c
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2021-12-20 14:59:15 +0100

    :robot: docs: Use codecov as test link.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/8134808bd3e9bcbb3e4b2ca5401e2dee58965a01/src/transforms/docs:use-codecov-as-test-link.js
    Please contact the author of the transform if you believe there was an error.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/graph-algorithm/minimum-cut';
+	testlink.href = 'https://app.codecov.io/gh/graph-algorithm/minimum-cut';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
